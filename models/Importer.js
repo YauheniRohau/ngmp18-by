d7b0a425d1082e2c46f2aba40f7eb46220b5ba6b
Yauheni Rohau <Yauheni_Rohau@epam.com>
2018-05-04 18:05:13 +0300

    Add importSync method

--- models/Importer.js
+++ models/Importer.js
@@ -6,11 +6,17 @@ export class Importer {
 
   listen = () => {
     DirWatcherEmitter.on('changed', (arrayOfFiles) => {
-      const jsonData = Promise.all(
-        arrayOfFiles.addedFiles
-          .map(file => this.import(`${arrayOfFiles.path}/${file}`))
-      );
-      jsonData.then(array => array.forEach(data => console.log(data)));
+      // const jsonData = Promise.all(
+      //   arrayOfFiles.addedFiles
+      //     .map(file => this.import(`${arrayOfFiles.path}/${file}`))
+      // );
+      // jsonData.then(array => array.forEach(data => console.log(data)));
+
+      const jsonData = arrayOfFiles.addedFiles
+        .map(file => {
+          const cb = (js) => js;
+          this.importSync(`${arrayOfFiles.path}/${file}`, js => console.log(js));
+        });
     });
   }
 
@@ -31,20 +37,18 @@ export class Importer {
     });
   }
 
-  // importSync = (path) => {
-  //   const resultJson = [];
-  //   csv()
-  //     .fromFile(path)
-  //     .on('json', (jsonObj) => {
-  //       resultJson.push(jsonObj);
-
-  //     })
-  //     .on('error', (error) => {
-  //       return error;
-  //     })
-  //     .on('done', () => {
-  //       console.log(resultJson);
-  //       return resultJson;
-  //     });
-  // }
+  importSync = (path, cb) => {
+    const resultJson = [];
+    csv()
+      .fromFile(path)
+      .on('json', (jsonObj) => {
+        resultJson.push(jsonObj);
+      })
+      .on('error', (error) => {
+        return error;
+      })
+      .on('done', () => {
+        cb(resultJson);
+      });
+  }
 }
