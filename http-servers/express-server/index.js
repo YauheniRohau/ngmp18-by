a02a529dacd4275f33cba7bc37540082df1f7508
Yauheni Rohau <Yauheni_Rohau@epam.com>
2018-07-16 13:19:22 +0300

    Start homework7

--- http-servers/express-server/index.js
+++ http-servers/express-server/index.js
@@ -1,18 +1,5 @@
 import app from './app';
 const port = process.env.PORT || 8080;
 
-//Models
-var models = require("./models");
-
-//Sync Database
-models.sequelize.sync().then(function() {
-
-  console.log('Nice! Database looks fine')
-  app.listen(port, () => console.log(`App listening on port ${port}!`));
-
-}).catch(function(err) {
-
-  console.log(err, "Something went wrong with the Database Update!")
-
-  });
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
